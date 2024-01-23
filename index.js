2c10d76d73a6160958eb85f348b65932c21c9002
Shuan0402 <st980155@gmail.com>
2024-01-24 00:30:31 +0800

    dialog animation

--- index.js
+++ index.js
@@ -1,32 +1,55 @@
 function styleCheckAll() {
-    var option=document.getElementsByName("style-checkbox");
+    let option=document.getElementsByName("style-checkbox");
     if (option[0].checked){
-        for (var i=1; i<option.length; i++){
+        for (let i=1; i<option.length; i++){
             option[i].checked=true;
         }
     }
     else{
-        for (var i=1; i<option.length; i++){
+        for (let i=1; i<option.length; i++){
             option[i].checked=false;
         }
     }
 }
 
 function categoryCheckAll() {
-    var option=document.getElementsByName("category-checkbox");
+    let option=document.getElementsByName("category-checkbox");
     if (option[0].checked){
-        for (var i=1; i<option.length; i++){
+        for (let i=1; i<option.length; i++){
             option[i].checked=true;
         }
     }
     else{
-        for (var i=1; i<option.length; i++){
+        for (let i=1; i<option.length; i++){
             option[i].checked=false;
         }
     }
 }
 
 function clickBlacklist(){
-    var modal=document.getElementById("blacklist-modal");
+    let modal=document.getElementById("blacklist-modal");
     modal.showModal();
 }
+
+function dialogClose(){
+    let dialogs=document.getElementsByClassName("filter-setting-modal");
+    Array.from(dialogs).forEach(dialog => {
+        dialog.addEventListener("click", e => {
+            const dialogDimensions = dialog.getBoundingClientRect()
+            if (e.clientX < dialogDimensions.left ||
+                e.clientX > dialogDimensions.right ||
+                e.clientY < dialogDimensions.top ||
+                e.clientY > dialogDimensions.bottom) {
+                
+                dialog.classList.add('hide');
+                dialog.addEventListener('animationend', function(){
+                    dialog.classList.remove('hide');
+                    dialog.close();
+                    dialog.removeEventListener('animationend',  arguments.callee, false);
+                }, false);
+            }
+        })
+    });
+}
+
+dialogClose();
